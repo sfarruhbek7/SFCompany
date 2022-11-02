d3d608900342d1ca5cdb696159d3dbbe29b59ae3
Farruhbek <111556129+sfarruhbek7@users.noreply.github.com>
2022-11-02 19:43:27 +0500

    Update newbot.php

--- newbot.php
+++ newbot.php
@@ -5,6 +5,5 @@ $telegram = new Telegram('5654583058:AAHPJUnyknyOEFpAQxgPjHYA6SrzIB7xoKM');
 $chat_id=$telegram->ChatID();
 $content=array('chat_id'=>$chat_id,'text'=>'SF componyiyasiga hush kelipsiz');
 $telegram->sendMessage($content);
-/kjgh
 //https://api.telegram.org/bot5654583058:AAHPJUnyknyOEFpAQxgPjHYA6SrzIB7xoKM/setWebhook?url=https://github.com/sfarruhbek7/SFCompany/blob/main/newbot.php
 ?>
