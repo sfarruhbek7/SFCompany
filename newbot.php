c36ef8f207cb9dfff1f89657069e10ee268bf283
Farruhbek <111556129+sfarruhbek7@users.noreply.github.com>
2022-11-02 19:39:36 +0500

    Add files via upload

---
+++ newbot.php
@@ -0,0 +1,10 @@
+<?php
+include 'Telegram.php';
+$telegram = new Telegram('5654583058:AAHPJUnyknyOEFpAQxgPjHYA6SrzIB7xoKM');
+
+$chat_id=$telegram->ChatID();
+$content=array('chat_id'=>$chat_id,'text'=>'SF componyiyasiga hush kelipsiz');
+$telegram->sendMessage($content);
+/kjgh
+//https://api.telegram.org/bot5654583058:AAHPJUnyknyOEFpAQxgPjHYA6SrzIB7xoKM/setWebhook?url=https://yoursite.com/your_update.php
+?>
